9777b71064643a8147d04d90d1d00ed29d529484
eunbi <geunbi38@gmail.com>
2022-04-14 22:18:20 +0900

    GameView 생성

---
+++ Project/app/src/main/java/kr/ac/tukorea/ge/smartphonegamep/runrunrun/GameView.java
@@ -0,0 +1,13 @@
+package kr.ac.tukorea.ge.smartphonegamep.runrunrun;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class GameView extends View {
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
